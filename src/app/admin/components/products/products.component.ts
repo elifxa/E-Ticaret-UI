de1b5e4ddbc3d3fdfb7a07fea7bd0afbf2d2a218
elifxa <elif.akcay@std.antalya.edu.tr>
2023-08-18 11:21:17 +0300

    fourth commit

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Product } from 'src/app/contracts/product';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
@@ -8,11 +10,36 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
-  constructor(spinner: NgxSpinnerService) {
+  constructor(
+    spinner: NgxSpinnerService,
+    private httpClientService: HttpClientService
+  ) {
     super(spinner);
   }
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallFadeRotating);
+
+    this.httpClientService
+      .get<Product[]>({ controller: 'products' })
+      .subscribe((data) => console.log(data));
+
+    // this.httpClientService
+    //   .post(
+    //     { controller: 'products' },
+    //     {
+    //       name: 'Kalem',
+    //       stock: 100,
+    //       price: 15,
+    //     }
+    //   )
+    //   .subscribe();
+
+    // this.httpClientService
+    //   .get({
+    //     baseUrl: 'https://jsonplaceholder.typicode.com',
+    //     controller: 'posts',
+    //   })
+    //   .subscribe((data) => console.log(data));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './admin/layout/layout.component';
@@ -10,9 +9,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { BaseComponent } from './base/base.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent], //,BaseComponent
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -21,8 +21,11 @@ import { BaseComponent } from './base/base.component';
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     NgxSpinnerModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: 'baseUrl', useValue: 'https://localhost:7213/api', multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
